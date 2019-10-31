bdaf2607fd06eb750f0b969cfcef4e7fc4ab9264
Kenneth Davis <kennethdavis391@gmail.com>
2019-10-31 13:10:49 -0400

    1-4

--- src/main/java/com/revature/eval/java/core/EvaluationService.java
+++ src/main/java/com/revature/eval/java/core/EvaluationService.java
@@ -140,8 +140,27 @@ public class EvaluationService {
 	 * @return
 	 */
 	public int getScrabbleScore(String string) {
-		// TODO Write an implementation for this method declaration
-		return 0;
+		string.toUpperCase();
+		int score = 0;
+		for (int i = 0; i < string.length(); i++) {
+			if (string.charAt(i) == 'A' || string.charAt(i) == 'E' || string.charAt(i) == 'I' || string.charAt(i) == 'O' || string.charAt(i) == 'U'
+			|| string.charAt(i) == 'L' || string.charAt(i) == 'N' || string.charAt(i) == 'R' || string.charAt(i) == 'S' || string.charAt(i) == 'T') {
+				score += 1;
+			}else if (string.charAt(i) == 'D' || string.charAt(i) == 'G') {
+				score += 2;
+			}else if (string.charAt(i) == 'B' || string.charAt(i) == 'C' || string.charAt(i) == 'M' || string.charAt(i) == 'P') {
+				score += 3;
+			}else if (string.charAt(i) == 'F' || string.charAt(i) == 'H' || string.charAt(i) == 'V' || string.charAt(i) == 'W' || string.charAt(i) == 'Y') {
+				score += 4;
+			}else if (string.charAt(i) == 'K') {
+				score += 5;
+			}else if (string.charAt(i) == 'J' ||string.charAt(i) == 'X') {
+				score += 8;
+			}else if (string.charAt(i) == 'Q' ||string.charAt(i) == 'Z') {
+				score +=10;
+			}
+		}
+		return score;
 	}
 
 	/**
